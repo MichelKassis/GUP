aeef90cbe1c3ec7d500f069c07cdabd701e3f726
Michel Kassis <michel.kassis120@gmail.com>
2020-01-10 21:11:36 -0500

    basic move

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+   
+    public class PlayerController : MonoBehaviour {
+        public float speed = 30;        // movement speed in units/sec
+       
+        void Update () {
+            Vector3 pos = transform.position;
+            pos.x += Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+            pos.y += Input.GetAxis("Vertical") * speed * Time.deltaTime;
+            transform.position = pos;
+        }
+         
+    }
+ 
