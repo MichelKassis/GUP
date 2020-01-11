e1899d2023618d2c393303abd7ea0eb60dccc1af
Michel Kassis <michel.kassis120@gmail.com>
2020-01-11 03:00:08 -0500

    prototype level (ipad ready)

--- Assets/Scripts/Equipable.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Equipable : Item
-{
-    public BodyPartType type;
-    public Vector2 equip_position;
-}
--- Assets/Scripts/Item.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Item : MonoBehaviour
-{
-    public float strength = 2;
-    public float mana = 5;
-}
