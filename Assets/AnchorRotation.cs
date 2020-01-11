c68c2180d755e6f693dc3b87221b73550198481c
Jack <monkeymanp8@gmail.com>
2020-01-11 12:20:27 -0500

    made alt rope system

---
+++ Assets/AnchorRotation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnchorRotation : MonoBehaviour
+{
+    public GameObject mech;
+    private Vector2 mech_position;
+    private Transform hold_transform;
+    // Update is called once per frame
+    void Update()
+    {
+        hold_transform = transform;
+        var aimAngle = Mathf.Atan2(transform.localPosition.y, transform.localPosition.x);
+        hold_transform.localEulerAngles = new Vector3(0, 0, aimAngle * Mathf.Rad2Deg);
+        transform.rotation = hold_transform.rotation;
+    }
+}
---
+++ Assets/fishyBalance.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fishyBalance : MonoBehaviour
+{
+    public GameObject mech_suit;
+    private Transform hold_transform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hold_transform = gameObject.transform;
+        hold_transform.localEulerAngles = new Vector3(0,0,-mech_suit.transform.eulerAngles.z);
+        gameObject.transform.rotation = hold_transform.rotation;
+    }
+}
