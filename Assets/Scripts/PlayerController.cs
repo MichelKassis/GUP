c02dc3672c77657346396cd0da544aca4f796a99
Michel Kassis <michel.kassis120@gmail.com>
2020-01-10 22:18:20 -0500

    raycasting (without cols)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,38 @@
    
     public class PlayerController : MonoBehaviour {
         public float speed = 30;        // movement speed in units/sec
-       
+        public Camera camera;
+        Rigidbody2D rb;
+        float movement = 0f;
+        void Start(){
+            rb = GetComponent<Rigidbody2D>();
+            //Camera = GameObject.Find("Main Camera");
+        }
+        
         void Update () {
             Vector3 pos = transform.position;
-            pos.x += Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-            pos.y += Input.GetAxis("Vertical") * speed * Time.deltaTime;
-            transform.position = pos;
+            movement += Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+            
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit    hit;
+            //transform.TransformDirection(Vector2.up)
+            Debug.DrawRay(transform.position, Input.mousePosition, Color.red);
+            if (Physics.Raycast(ray, out hit)) {
+            Transform objectHit = hit.transform;
+            Debug.DrawRay(transform.position, Input.mousePosition, Color.green);
+            // Do something with the object that was hit by the raycast.
+            //Vector3   direction = (hit.point - transform.position);
         }
+
+        }
+
+        void FixedUpdate()
+	    {
+            Vector2 velocity = rb.velocity;
+            velocity.x = movement;
+            rb.velocity = velocity;
+	    }
+
          
     }
  
