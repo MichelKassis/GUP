78a48472869c9e97af3129f9cb0746b27a8b18e7
Michel Kassis <michel.kassis120@gmail.com>
2020-01-11 04:17:55 -0500

    water level

---
+++ Assets/Scripts/EndlessRun/ERPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ERPlayer : MonoBehaviour {
+
+    public EndlessRun er;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left *5*Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * 5 * Time.deltaTime);
+        }
+    }
+
+    //If you use only2D object use OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("Obstacles"))
+            er.isGameOver = true;
+    }
+}
---
+++ Assets/Scripts/EndlessRun/EndlessRun.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndlessRun : MonoBehaviour {
+
+    public List<GameObject> bgList;
+    public List<Transform> currentManagedBGList;
+    public GameObject lastSpawnedBG;
+    public bool isGameOver = false;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        if(!isGameOver)
+		    foreach(Transform t in currentManagedBGList)
+            {
+                t.transform.Translate(Vector3.down * 5 * Time.deltaTime);
+            }
+	}
+
+    //If you use only2D object use OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag.Equals("BG"))
+        {
+            currentManagedBGList.Remove(other.transform);
+            Destroy(other.gameObject);
+
+            Vector3 spawnPos = lastSpawnedBG.transform.position;
+            spawnPos.y += lastSpawnedBG.GetComponent<Collider>().bounds.size.y/2;
+
+
+            lastSpawnedBG = Instantiate(bgList[Random.Range(0, bgList.Count)], spawnPos,Quaternion.identity);
+            spawnPos.y += lastSpawnedBG.GetComponent<Collider>().bounds.size.y / 2;
+            lastSpawnedBG.transform.position = spawnPos;
+
+            currentManagedBGList.Add(lastSpawnedBG.transform);
+        }
+    }
+}
