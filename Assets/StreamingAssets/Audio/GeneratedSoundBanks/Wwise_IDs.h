c5f6c29bcef68caf76ba9ffe1e33cefaa51f75bc
KillerPlankton <36796176+KillerPlankton@users.noreply.github.com>
2020-01-12 05:16:45 -0500

    add soundbank

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,66 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID BIRDS_HURT = 3811936753U;
+        static const AkUniqueID BOSS = 1560169506U;
+        static const AkUniqueID DIED = 2514411491U;
+        static const AkUniqueID END = 529726532U;
+        static const AkUniqueID HAWK_SCREAM = 2122105434U;
+        static const AkUniqueID HURT = 3193947170U;
+        static const AkUniqueID INITGAME = 4113816833U;
+        static const AkUniqueID LEVEL2 = 2678230381U;
+        static const AkUniqueID MENU = 2607556080U;
+        static const AkUniqueID MENU_QUIT = 2776193354U;
+        static const AkUniqueID STOPMUSIC = 1917263390U;
+        static const AkUniqueID WEB_ATTACK = 3903201844U;
+        static const AkUniqueID WEB_GRIP = 2524867556U;
+        static const AkUniqueID WEB_LAUNCH = 1335561985U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace PROGRESSION
+        {
+            static const AkUniqueID GROUP = 1957646882U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID LEVEL1 = 2678230382U;
+                static const AkUniqueID LEVEL2 = 2678230381U;
+            } // namespace SWITCH
+        } // namespace PROGRESSION
+
+    } // namespace SWITCHES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID GUP = 915214423U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
