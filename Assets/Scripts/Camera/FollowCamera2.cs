3ae05dfab8183ef209f3e533294fc82d5067ac74
Michel Kassis <michel.kassis120@gmail.com>
2020-01-11 19:39:16 -0500

    Camera fixed (clamped x axis)

--- Assets/Scripts/Camera/FollowCamera2.cs
+++ Assets/Scripts/Camera/FollowCamera2.cs
@@ -6,30 +6,28 @@ public class FollowCamera2 : MonoBehaviour
 {
     public Transform target;
 
-    public Vector2 offset = Vector2.up * 5;
+    public Vector3 offset = Vector3.up * 5;
     public float speed = 2;
     public float size = 20;
     public float min_dist = 0.5f;
     
     public float minX = 4.1f;
     public float maxX = -3.9f;
-    public float minY = 14.0f;
+    public float minY = 54.0f;
     public float maxY = 100.0f;
    
     void Start()
     {
         Camera cam = GetComponent<Camera>();
         //cam.orthographicSize = size;
+        target = GameObject.Find("Fish").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 dir = target.position - transform.position;
-        dir += offset;
-        if (dir.sqrMagnitude > min_dist * min_dist)
-        {   Vector3 yPosition = (Vector3)dir * speed * Time.deltaTime;
-            transform.position += (Vector3)dir * speed * Time.deltaTime;
+        if(target.position.y > minY){
+        transform.position = new Vector3(transform.position.x, target.position.y, -10);
         }
     }
 }
