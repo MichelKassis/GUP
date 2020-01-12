f1d4f4bf2c6984c10b30c6388eb8d3dedb97b3e4
Jeffery <jeffery124@gmail.com>
2020-01-12 14:40:28 -0500

    ???

---
+++ Assets/Scripts/Enemies/EnemyReference.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyReference : MonoBehaviour
+{
+    public PolygonCollider2D mainCharacter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
